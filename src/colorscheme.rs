da600141205eea8fc3b9772ceb2a57eff8925725
aig77 <29128253+aig77@users.noreply.github.com>
2024-07-13 15:31:04 -0400

    fold distance instead of using for loop

--- src/colorscheme.rs
+++ src/colorscheme.rs
@@ -41,57 +41,35 @@ impl ColorScheme {
         self.scheme.len()
     }
 
-    pub fn euclidean_distance(&self, target_scheme: &ColorScheme) -> f64 {
-        let mut sum_squared_diff: f64 = 0.0;
-        for i in 0..8 {
-            let self_norm = Rgb {
-                r: self.scheme[i].r as f64 / 255.0,
-                g: self.scheme[i].g as f64 / 255.0,
-                b: self.scheme[i].b as f64 / 255.0
-            };
+    pub fn euclidean_distance_with_weights(&self, other: &ColorScheme, weights: &[f64]) -> f64 {
+        let sum_squared_diff = self.scheme.iter().enumerate()
+            .fold(0.0, | acc, (i, self_color) | {
+                let self_norm = Rgb {
+                    r: self_color.r as f64 / 255.0,
+                    g: self_color.g as f64 / 255.0,
+                    b: self_color.b as f64 / 255.0
+                };
 
-            let target_norm = Rgb {
-                r: target_scheme.scheme[i].r as f64 / 255.0,
-                g: target_scheme.scheme[i].g as f64 / 255.0,
-                b: target_scheme.scheme[i].b as f64 / 255.0
-            };
+                let other_color = &other.scheme[i];
+                let other_norm = Rgb {
+                    r: other_color.r as f64 / 255.0,
+                    g: other_color.g as f64 / 255.0,
+                    b: other_color.b as f64 / 255.0
+                };
 
-            let diff_r = (self_norm.r - target_norm.r).powi(2);
-            let diff_g = (self_norm.g - target_norm.g).powi(2);
-            let diff_b = (self_norm.b - target_norm.b).powi(2);
-            sum_squared_diff += diff_r + diff_g + diff_b;
-        }
+                let diff_r = (self_norm.r - other_norm.r).powi(2) * weights[i];
+                let diff_g = (self_norm.g - other_norm.g).powi(2) * weights[i];
+                let diff_b = (self_norm.b - other_norm.b).powi(2) * weights[i];
 
+                acc + diff_r + diff_g + diff_b
+            });
+        
         sum_squared_diff.sqrt()
+        
     }
 
-    pub fn euclidean_distance_with_weights(&self, target_scheme: &ColorScheme, weights: &[f64]) -> f64 {
-        let self_colors = &self.scheme;
-        let target_colors = &target_scheme.scheme;
-
-        let num_colors_to_compare = std::cmp::min(self_colors.len(), target_colors.len());
-
-        let mut sum_squared_diff = 0.0;
-        for i in 0..num_colors_to_compare {
-            let self_norm = Rgb {
-                r: self.scheme[i].r as f64 / 255.0,
-                g: self.scheme[i].g as f64 / 255.0,
-                b: self.scheme[i].b as f64 / 255.0
-            };
-
-            let target_norm = Rgb {
-                r: target_scheme.scheme[i].r as f64 / 255.0,
-                g: target_scheme.scheme[i].g as f64 / 255.0,
-                b: target_scheme.scheme[i].b as f64 / 255.0,
-            };
-
-            let diff_r = (self_norm.r - target_norm.r).powi(2) * weights[i];
-            let diff_g = (self_norm.g - target_norm.g).powi(2) * weights[i];
-            let diff_b = (self_norm.b - target_norm.b).powi(2) * weights[i];
-            sum_squared_diff += diff_r + diff_g + diff_b;
-        }
-
-        sum_squared_diff.sqrt()
+    pub fn euclidean_distance(&self, other: &ColorScheme) -> f64 {
+        self.euclidean_distance_with_weights(other, &vec![1.0; self.len()])
     }
 }
 
@@ -104,7 +82,7 @@ impl fmt::Display for ColorScheme {
             result.push_str(&format!("{}   \x1b[0m", escape_code));
         }
 
-        write!(f, "{}", result);
+        write!(f, "{}", result)?;
 
         Ok(())
     }   
