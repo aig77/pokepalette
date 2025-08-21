fafc110adf6bfee4ae41ad860c52bd8f45841a2f
Arturo <git.lunchroom670@slmails.com>
2025-08-20 21:44:04 -0400

    allow dead code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+#[allow(dead_code)]
 mod sprite;
+
+#[allow(dead_code)]
+mod quantize;
 
 use serde_json;
 use sprite::Sprite;
@@ -9,16 +13,10 @@ const DB_PATH: &str = "assets/pokemon.json";
 fn main() {
     let file = fs::File::open(DB_PATH).expect("Failed to open pokemon.json");
 
-    let sprites: Vec<Sprite> = serde_json::from_reader(file).expect("Failed to parse pokemon.json");
+    let _sprites: Vec<Sprite> =
+        serde_json::from_reader(file).expect("Failed to parse pokemon.json");
 
     // for sprite in sprites {
     // println!("{}", sprite);
     // }
-
-    let palette = match extract_palette_from_image("black-hole.png") {
-        Ok(p) => p,
-        Err(e) => panic!("{}", e),
-    };
-
-    println!("{:?}", palette);
 }
