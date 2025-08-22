a86e178bb69720d90284850011fa27016d6790f1
Arturo <29128253+aig77@users.noreply.github.com>
2025-08-22 18:16:17 -0400

    turn into cli tool

--- src/distance.rs
+++ src/distance.rs
@@ -1,39 +1,29 @@
 #![allow(dead_code)]
 
 use crate::quantize::WeightedColor;
-use anyhow::{anyhow, Result};
 use lab::Lab;
 
-pub fn palette_distance(
-    palette_a: &Vec<WeightedColor>,
-    palette_b: &Vec<WeightedColor>,
-) -> Result<f32> {
-    let distance_a_to_b = directional_distance(palette_a, palette_b)?;
-    let distance_b_to_a = directional_distance(palette_b, palette_a)?;
+pub fn palette_distance(palette_a: &Vec<WeightedColor>, palette_b: &Vec<WeightedColor>) -> f32 {
+    let distance_a_to_b = directional_distance(palette_a, palette_b);
+    let distance_b_to_a = directional_distance(palette_b, palette_a);
 
-    Ok((distance_a_to_b + distance_b_to_a) / 2.0)
+    (distance_a_to_b + distance_b_to_a) / 2.0
 }
 
-fn directional_distance(
-    from_palette: &Vec<WeightedColor>,
-    to_palette: &Vec<WeightedColor>,
-) -> Result<f32> {
+fn directional_distance(from_palette: &Vec<WeightedColor>, to_palette: &Vec<WeightedColor>) -> f32 {
     let mut total_weighted_distance = 0.0;
 
     for wc_from in from_palette {
         let min_distance = to_palette
             .iter()
             .map(|wc_to| euclidean_distance(&wc_from.color, &wc_to.color))
-            .min_by(|a, b| {
-                a.partial_cmp(b)
-                    .ok_or_else(|| anyhow!("NaN in distance calculation"))?
-            })
+            .min_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal))
             .unwrap_or(f32::MAX);
 
         total_weighted_distance += min_distance * wc_from.freq;
     }
 
-    Ok(total_weighted_distance)
+    total_weighted_distance
 }
 
 fn euclidean_distance(color1: &[u8; 3], color2: &[u8; 3]) -> f32 {
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ pub mod sprite;
 pub const DEFAULT_PALETTE_SIZE: usize = 5;
 pub const DEFAULT_LEVELS: usize = 8;
 pub const DEFAULT_IGNORE_BLACK: bool = true;
+pub const DEFAULT_TOP_K: usize = 10;
