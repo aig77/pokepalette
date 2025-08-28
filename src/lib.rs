3b15452d0d675ec9d770e11c62ab8372f66c3e6d
Arturo <git.lunchroom670@slmails.com>
2025-08-27 22:27:08 -0400

    convert db to bin for faster loading

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub use cli::output::{print_image_information, print_result, print_top_informati
 pub use colorquant::{get_palette, palette_distance, WeightedColor};
 
 pub const KRABBY_BASE_URL: &str = "https://raw.githubusercontent.com/yannjor/krabby/main/";
-pub const DB_PATH: &str = "pokemon.json";
+pub const DB_FILE_NAME: &str = "pokemon.bin";
 pub const DEFAULT_PALETTE_SIZE: usize = 5;
 pub const DEFAULT_LEVELS: usize = 8;
 pub const DEFAULT_IGNORE_BLACK: bool = true;
