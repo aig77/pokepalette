d349d8874bd9f604fc40d3f9634216c400b0c380
Arturo <git.lunchroom670@slmails.com>
2025-08-29 00:22:18 -0400

    pokemon database module

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,4 +1,3 @@
-use crate::sprite::{Form, Sprite};
 use crate::DEFAULT_TOP_K;
 use clap::{ArgGroup, Parser};
 
@@ -15,93 +14,43 @@ use clap::{ArgGroup, Parser};
         .args(["no_gmax", "all_gmax"])
         .multiple(false)),
 )]
-struct Args {
+pub struct Args {
     /// Path of the image
-    image: String,
+    pub image: String,
 
     /// Number of pokemon returned
     #[arg(short, long, default_value_t = DEFAULT_TOP_K)]
-    top_k: usize,
+    pub top_k: usize,
 
     /// Print additional information
     #[arg(short, long, default_value_t = false)]
-    verbose: bool,
+    pub verbose: bool,
 
     /// Filter out shinies
     #[arg(long, default_value_t = false)]
-    no_shiny: bool,
+    pub no_shiny: bool,
 
     /// Filter out non-shinies
     #[arg(long, default_value_t = false)]
-    all_shiny: bool,
+    pub all_shiny: bool,
 
     /// Filter out mega
     #[arg(long, default_value_t = false)]
-    no_mega: bool,
+    pub no_mega: bool,
 
     /// Filter out non-mega
     #[arg(long, default_value_t = false)]
-    all_mega: bool,
+    pub all_mega: bool,
 
     /// Filter out gmax
     #[arg(long, default_value_t = false)]
-    no_gmax: bool,
+    pub no_gmax: bool,
 
     /// Filter out non-gmax
     #[arg(long, default_value_t = false)]
-    all_gmax: bool,
+    pub all_gmax: bool,
 
     /// Filter out regional variants
     #[arg(long, default_value_t = false)]
-    no_regional: bool,
-}
-
-pub struct Config {
-    pub image: String,
-    pub top_k: usize,
-    pub verbose: bool,
-}
-
-impl Config {
-    fn new(args: &Args) -> Self {
-        Config {
-            image: args.image.clone(),
-            top_k: args.top_k,
-            verbose: args.verbose,
-        }
-    }
-}
-
-pub fn get_config_and_filter_sprites(sprites: Vec<Sprite>) -> (Config, Vec<Sprite>) {
-    let args = Args::parse();
-    let config = Config::new(&args);
-    let filtered = filter_sprites(sprites, &args);
-    (config, filtered)
-}
-
-fn filter_sprites(sprites: Vec<Sprite>, args: &Args) -> Vec<Sprite> {
-    sprites
-        .into_iter()
-        .filter(|sprite| {
-            let shiny_condition =
-                (!args.no_shiny || !sprite.shiny) && (!args.all_shiny || sprite.shiny);
-
-            let mega_condition = {
-                let is_mega = matches!(sprite.form, Form::Mega(_));
-                (!args.no_mega || !is_mega) && (!args.all_mega || is_mega)
-            };
-
-            let gmax_condition = {
-                let is_gmax = matches!(sprite.form, Form::Gmax);
-                (!args.no_gmax || !is_gmax) && (!args.all_gmax || is_gmax)
-            };
-
-            let regional_condition = {
-                let is_regional = matches!(sprite.form, Form::Regional(_));
-                !args.no_regional || !is_regional
-            };
-
-            shiny_condition && mega_condition && gmax_condition && regional_condition
-        })
-        .collect()
+    pub no_regional: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,29 @@
 use anyhow::Result;
-use bincode;
-
+use clap::Parser;
+use pokepalette::cli::args::Args;
 use pokepalette::colorquant::{get_image_palette, get_pokemon_ranked};
+use pokepalette::pokemondatabase::PokemonDatabase;
 use pokepalette::sprite::Sprite;
-use pokepalette::DB_FILE_NAME;
-use pokepalette::{
-    get_config_and_filter_sprites, print_image_information, print_result, print_top_information,
-};
+use pokepalette::{print_image_information, print_result, print_top_information};
 
 fn main() -> Result<()> {
-    // Load database
-    let binary_data = std::fs::read(DB_FILE_NAME)?;
-    let (sprites, _): (Vec<Sprite>, usize) =
-        bincode::serde::decode_from_slice(&binary_data, bincode::config::standard())?;
+    let args = Args::parse();
 
-    // Load CLI config and filters
-    let (config, filtered) = get_config_and_filter_sprites(sprites);
+    // Load database and filter
+    let database = PokemonDatabase::load()?;
+    let sprites = database.filtered(&args);
 
     // Generate image palette
-    let image_palette = get_image_palette(&config.image)?;
+    let image_palette = get_image_palette(&args.image)?;
 
     // Get sprites sorted by distance to image
-    let ranked = get_pokemon_ranked(&image_palette, &filtered);
+    let ranked = get_pokemon_ranked(&image_palette, &sprites);
 
     // Get top k
-    let top: Vec<(&Sprite, f32)> = ranked.into_iter().take(config.top_k).collect();
+    let top: Vec<(&Sprite, f32)> = ranked.into_iter().take(args.top_k).collect();
 
     // Print results
-    if config.verbose {
+    if args.verbose {
         print_image_information(image_palette);
         print_top_information(&top);
     } else {
